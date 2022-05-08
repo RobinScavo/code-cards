9c86105fb73845842bceace1cdc3646aa5a77663
Robin Scavo <sirscavo@gmail.com>
2022-05-07 21:37:07 -0600

    endpoints complete

--- server/app.js
+++ server/app.js
@@ -23,12 +23,17 @@ connectToDb((err) => {
 
 
 // routes
-app.get('/', (req, res) => {
+app.get('/publicDecks', (req, res) => {
+    const page = req.query.p || 0
+    const decksPerPage = 20
+
     let decks = []
 
     db.collection('publicDecks')
         .find() // cursor toArray forEach
         .sort({ likes: 1 })
+        .skip(page * decksPerPage)
+        .limit(decksPerPage)
         .forEach(deck => decks.push(deck))
         .then(() => {
             res.status(200).json(decks);
@@ -39,11 +44,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/privateDecks', (req, res) => {
+    const page = req.query.p || 0
+    const decksPerPage = 20
+
     let decks = []
 
     db.collection('privateDecks')
         .find() // cursor toArray forEach
         // .sort({ likes: 1 })
+        .skip(page * decksPerPage)
+        .limit(decksPerPage)
         .forEach(deck => decks.push(deck))
         .then(() => {
             res.status(200).json(decks);
@@ -94,4 +104,64 @@ app.post('/privateDecks', (req, res) => {
     .catch(err => {
         res.status(500).json({err: 'Could not create a new deck'})
     })
+})
+
+app.post('/publicDecks', (req, res) => {
+    const deck = req.body
+
+    db.collection('publicDecks')
+    .insertOne(deck)
+    .then(result => {
+        res.status(201).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({err: 'Could not create a new deck'})
+    })
+})
+
+app.delete('/privateDecks/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('privateDecks')
+            .deleteOne({ _id: ObjectId(req.params.id) })
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'Could not delete deck.'})
+            })
+    } else {
+        res.status(500).json({error: 'Not a valid deck ID.'})
+    }
+})
+
+app.delete('/publicDecks/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('publicDecks')
+            .deleteOne({ _id: ObjectId(req.params.id) })
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'Could not delete deck.'})
+            })
+    } else {
+        res.status(500).json({error: 'Not a valid deck ID.'})
+    }
+})
+
+app.patch('/privateDecks/:id', (req, res) => {
+    const updates = req.body
+
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('privateDecks')
+            .updateOne({ _id: ObjectId(req.params.id)}, {$set: updates} )
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'Could not update deck.'})
+            })
+    } else {
+        res.status(500).json({error: 'Not a valid deck ID.'})
+    }
 })
